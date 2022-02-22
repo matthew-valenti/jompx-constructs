a3110700e5aad0940f0b7b441d8e68abb2f8a561
Matthew Valenti <matthew@aydus.com>
2022-02-21 21:12:42 -0800

    feat(pipeline): mvp

--- lib/constructs/cdk-pipeline.construct.d.ts
+++ lib/constructs/cdk-pipeline.construct.d.ts
@@ -7,6 +7,8 @@ export interface IJompxCdkPipelineProps {
  * Deploy in parallel? READ THIS: https://docs.aws.amazon.com/cdk/api/v1/docs/pipelines-readme.html
  * Continuous integration and delivery (CI/CD) using CDK Pipelines: https://docs.aws.amazon.com/cdk/v2/guide/cdk_pipeline.html
  * CDK doco: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.pipelines-readme.html
+ * Build Spec Reference: https://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html
+ * nx cicd: https://nx.dev/ci/monorepo-ci-circle-ci
  *
  * Trigger apps pipeline??? https://stackoverflow.com/questions/62857925/how-to-invoke-a-pipeline-based-on-another-pipeline-success-using-aws-codecommit
  */
--- src/constructs/cdk-pipeline.construct.ts
+++ src/constructs/cdk-pipeline.construct.ts
@@ -1,7 +1,6 @@
-import * as cdk from 'aws-cdk-lib';
+// import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as pipelines from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-import { IEnvironment, Stage } from '../classes/environment';
 
 export interface IJompxCdkPipelineProps {
     shellStepInput: pipelines.IFileSetProducer;
@@ -11,6 +10,8 @@ export interface IJompxCdkPipelineProps {
  * Deploy in parallel? READ THIS: https://docs.aws.amazon.com/cdk/api/v1/docs/pipelines-readme.html
  * Continuous integration and delivery (CI/CD) using CDK Pipelines: https://docs.aws.amazon.com/cdk/v2/guide/cdk_pipeline.html
  * CDK doco: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.pipelines-readme.html
+ * Build Spec Reference: https://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html
+ * nx cicd: https://nx.dev/ci/monorepo-ci-circle-ci
  *
  * Trigger apps pipeline??? https://stackoverflow.com/questions/62857925/how-to-invoke-a-pipeline-based-on-another-pipeline-success-using-aws-codecommit
  */
@@ -25,16 +26,10 @@ export class JompxCdkPipeline extends Construct {
             crossAccountKeys: true, // Required for cross account deploys.
             synth: new pipelines.ShellStep('Synth', {
                 input: props.shellStepInput,
-                commands: ['npm install', 'npm -g install typescript', 'npm install -g nx', 'cd apps/cdk', 'npm run build', 'npx cdk synth'], // AWS docs example commands: ['npm ci', 'npm run build', 'npx cdk synth']
+                commands: ['ls', 'npm install', 'npm -g install typescript', 'npm install -g nx', 'nx build cdk', 'nx synth cdk'], // AWS docs example commands: ['npm ci', 'npm run build', 'npx cdk synth']
                 primaryOutputDirectory: 'apps/cdk/cdk.out'
             })
         });
-
-        // TODO: Consider moving the commands to a separate script file? Not sure if this helps or hurts us.
-        // stage.addPost(new ShellStep('validate', {
-        //     input: source,
-        //     commands: ['sh ./tests/validate.sh']
-        // }));
     }
 }
 
