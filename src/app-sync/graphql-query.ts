eac7b6f2800e3407fefbddcafecfd1445c51fab3
Matthew Valenti <matthew@aydus.com>
2022-05-13 16:14:59 -0700

    feat(appsync): poc

--- src/app-sync/graphql-query.ts
+++ src/app-sync/graphql-query.ts
@@ -1,24 +1,24 @@
-import * as changeCase from 'change-case';
-import * as graphql from 'graphql';
-import gql from 'graphql-tag';
+// import * as changeCase from 'change-case';
+// import * as graphql from 'graphql';
+// import gql from 'graphql-tag';
 
-export class GraphqlQuery {
+// export class GraphqlQuery {
 
-    public static find(typeName: string, fieldsFragment: graphql.DocumentNode): graphql.DocumentNode {
+//     public static find(typeName: string, fieldsFragment: graphql.DocumentNode): graphql.DocumentNode {
 
-        const queryName = `${changeCase.camelCase(typeName)}Find`;
+//         const queryName = `${changeCase.camelCase(typeName)}Find`;
 
-        return gql`query ${queryName} (
-                $filter: AWSJSON
-                $limit: Int
-                $skip: Int
-                $sort: [SortInput]
-            ) {
-                ${queryName}(filter: $filter, limit: $limit, skip: $skip, sort: $sort) {
-                    ...fields
-                }
-            }
-            ${fieldsFragment}
-        `;
-    }
-}
+//         return gql`query ${queryName} (
+//                 $filter: AWSJSON
+//                 $limit: Int
+//                 $skip: Int
+//                 $sort: [SortInput]
+//             ) {
+//                 ${queryName}(filter: $filter, limit: $limit, skip: $skip, sort: $sort) {
+//                     ...fields
+//                 }
+//             }
+//             ${fieldsFragment}
+//         `;
+//     }
+// }
